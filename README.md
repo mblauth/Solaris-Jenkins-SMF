c7d6b8aa4f6caa6496dc672d7246b51f715a78c6
Marvin Blauth <marvin@slatanic.org>
2021-03-06 13:00:06 +0100

    Add support for Solaris 10

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a SMF configuration for running a Jenkins agent on Solaris
+This is an SMF configuration for running a Jenkins agent on Solaris
 
 For installation follow these steps:
 
@@ -9,6 +9,7 @@ For installation follow these steps:
 5. Configure Jenkins master URL by running `svccfg -s site/jenkins setprop jenkins/url=<Jenkins master URL>`.
 6. Configure agent's working directory by running `svccfg -s site/jenkins setprop jenkins/workDir=<Jenkins agent working directory>`, defaults to /opt/jenkins.
 7. Configure secret by running `svccfg -s site/jenkins setprop jenkins/secret=<secret>`.
+8. Configure Java executable (optional) by running `svccfg -s site/jenkins setprop jenkins/javaExecutable=<path to java executable>`.
 8. Ensure you have a user called "jenkins" with write access to the working directory.
 9. Start the service with `svcadm enable jenkins`
 10. Check status with `svcs -l jenkins`
--- jenkins.sh
+++ jenkins.sh
@@ -4,19 +4,24 @@
 
 getproparg() {
  arg=$(svcprop -p $1 $SMF_FMRI)
- [ -n "$arg" ] && echo $arg
+ [[ -n "$arg" && $arg != '""' ]] && echo $arg # Solaris 10 returns "", Solaris 11 nothing
 }
 
-agentJar="$(getproparg jenkins/agentJar)"
-url="$(getproparg jenkins/url)"
-workDir="$(getproparg jenkins/workDir)"
-secret="$(getproparg jenkins/secret)"
+agentJar=$(getproparg jenkins/agentJar)
+url=$(getproparg jenkins/url)
+workDir=$(getproparg jenkins/workDir)
+secret=$(getproparg jenkins/secret)
+javaExecutable=$(getproparg jenkins/javaExecutable)
 
 fail() {
     echo $1
-    exit 1
+    exit $SMF_EXIT_FATAL
 }
 
-([ -z "$agentJar" ] || [ -z "$url" ] || [ -z "$workDir" ] || [ -z "$secret" ]) && fail "not all required properties were set"
 
-java -jar $agentJar -jnlpUrl $url -secret $secret -workDir $workDir
+[[ -z "$agentJar" || -z "$url" || -z "$workDir" || -z "$secret" ]] && fail "not all required properties were set" ]]
+
+[ -z $javaExecutable ] && javaExecutable=java
+
+$javaExecutable -jar "$agentJar" -jnlpUrl "$url" -secret "$secret" -workDir "$workDir"
+exit $SMF_EXIT_OK
